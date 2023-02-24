192f5d699e42e1a913fd9bf58ab67d34e4405b6a
Shardin Nikita <shardin@greendatasoft.ru>
2023-02-24 17:05:03 +0500

    to links from const

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink } from 'shared/ui/AppLink';
 import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routePath/routePath';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -12,17 +13,16 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
 
     return (
-
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink
-                    to="/"
+                    to={RoutePath.main}
                     variant="secondary"
                 >
                     {t('Main')}
                 </AppLink>
                 <AppLink
-                    to="/about"
+                    to={RoutePath.about}
                     variant="secondary"
                 >
                     {t('About')}
