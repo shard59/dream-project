9af4952f377507bd19db0cc21077583c0af0640b
Shardin Nikita <shardin@greendatasoft.ru>
2023-02-25 20:25:43 +0500

    fix unit tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { screen } from '@testing-library/react';
-import renderWithTranslation from 'shared/helpers/tests/renderWithTranslation';
 import { fireEvent } from '@storybook/testing-library';
 import Sidebar from './Sidebar';
+import componentRender from "shared/helpers/tests/componentRender";
 
 describe('Sidebar', () => {
     test('Render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Collapse sidebar', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
