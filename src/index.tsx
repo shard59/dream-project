37bfa38296e1746a35ee03e90905bff18eb1fad5
Shardin Nikita <shardin@greendatasoft.ru>
2023-04-17 16:07:25 +0500

    redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,19 @@ import { render } from 'react-dom';
 import App from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-
 import 'shared/config/i18n/i18n';
 import ErrorBoundary from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
